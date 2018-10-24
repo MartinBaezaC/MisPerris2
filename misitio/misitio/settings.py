67a46f142bafc4e293cce725cb13014bcc1c55a7
martin <martin@correo.cl>
2018-10-24 18:16:23 -0300

    Se agrego la clase perro al modelo de base de datos

--- misitio/misitio/settings.py
+++ misitio/misitio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'perris.apps.PerrisConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- misitio/perris/models.py
+++ misitio/perris/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Perro(models.Model):
+	nombre = models.CharField(max_length=30)
+	raza = models.CharField(max_length=30)
+	descripcion = models.CharField(max_length=60)
+	ESTADOS=(('R','Rescatado'),('D','Disponible'),('A','Adoptado'))
+	estado = models.CharField(max_length=1, choices=ESTADOS,default='R')
 # Create your models here.
