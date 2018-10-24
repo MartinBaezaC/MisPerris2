c6606c88842123d70ababfbe51b384322dd41449
martin <martin@correo.cl>
2018-10-24 19:29:33 -0300

    Se agrego la ruta a la pagina web y formulario

--- misitio/perris/urls.py
+++ misitio/perris/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:perro_id>/', views.detalle, name='detalle'),
+    path('inicio', views.cargarInicio, name='cargarInicio'),
+    path('inicio/formulario', views.cargarFormulario, name='cargarFormulario'),
 ]
--- misitio/perris/views.py
+++ misitio/perris/views.py
@@ -5,4 +5,14 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hola mundo. Aplicación perris.")
+
+def detalle(request, perro_id):
+    return HttpResponse("You're looking at dog %s." % perro_id)
+
+def cargarInicio(request):
+    return render(request, 'perris/index.html')
+
+def cargarFormulario(request):
+    return render(request, 'perris/formulario.html')
+
 # Create your views here.
