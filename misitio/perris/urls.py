04a70e65b4b4d0f6842c03b28069c55d9313a043
ramiro <ramiro@correo.cl>
2018-11-20 00:18:05 -0300

    Se agrego api interactivo para misperris

--- misitio/perris/urls.py
+++ misitio/perris/urls.py
@@ -1,6 +1,33 @@
-from django.urls import path
+from django.urls import path , include
+
+from .models import Perro, Usuario
+from rest_framework import routers, serializers, viewsets
 
 from . import views
+
+class PerroSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Perro
+        fields = ('url', 'nombre', 'raza', 'descripcion')
+
+# ViewSets define the view behavior.
+class PerroViewSet(viewsets.ModelViewSet):
+    queryset = Perro.objects.all()
+    serializer_class = PerroSerializer
+
+class UsuarioSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ('url', 'nombre_completo', 'correo', 'run')
+
+# ViewSets define the view behavior.
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+router = routers.DefaultRouter()
+router.register(r'perros', PerroViewSet)
+router.register(r'usuarios', UsuarioViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -14,6 +41,7 @@ urlpatterns = [
 
     path('inicio/listadoRescatados', views.cargarRescatados, name='cargarRescatados'),
 
-    
+    path('mi-api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
     
 ]
